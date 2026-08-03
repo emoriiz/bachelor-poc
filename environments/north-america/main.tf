30f1e3d5d04779addd7ea88dc1484619bdc169f9
emoriiz <nico.kress1999@gmail.com>
2026-08-03 11:58:57 +0200

    add vpn-gateway and private endpoints (not finished yet)

--- environments/north-america/main.tf
+++ environments/north-america/main.tf
@@ -19,8 +19,7 @@ module "network" {
   address_space       = ["10.204.8.0/21"]
   subnets = {
     gateway  = ["10.204.8.0/24"]
-    server   = ["10.204.9.0/24"]
-    services = ["10.204.10.0/24"]
+    services = ["10.204.9.0/24"]
   }
   tags = local.common_tags
 }
@@ -31,8 +30,8 @@ module "vpn-gateway" {
   datacenter_location = var.datacenter_location
   resource_group_name = module.resource_groups.resource_group_names["network"]
   gateway_subnet_id   = module.network.gateway_subnet_id
-  # vpn_client_address_pool = ["172.16.201.0/24"]     # optional, wenn P2S konfiguriert
-  # vpn_root_cert_data      = file("./certs/rootCA.pem") # optional für P2S
+  vpn_client_address_pool = ["172.16.201.0/24"]
+  vpn_root_cert_data      = file("./certs/rootCA.pem")
   tags = local.common_tags
 }
 
